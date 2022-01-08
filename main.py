ed16b633d60f63c96a3d0bb3af2493532d0abb5f
Vishwarrior26 <srimathkandali@gmail.com>
2022-01-08 12:44:17 -0800

    Basic console input

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ change = {'C': 'G', 'C#': 'G#', 'Db': 'Ab',
           'E': 'B', 'F': 'C', 'F#': 'C#',
           'Gb': 'Db', 'G': 'D', 'G#': 'D#',
           'Ab': 'Eb', 'Bb': 'F', 'B': 'F#'}
-str = "E E F# G C B"
+# str = "E E F# G C B"
+print("Enter the concert pitches")
+str = input()
 for x in str.split(' '):
     print(change.get(x))
