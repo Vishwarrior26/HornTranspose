af57767aac8bdb0fe07a9ac4e4586c96fbdaca0a
Vishwarrior26 <srimathkandali@gmail.com>
2022-01-08 12:57:35 -0800

    Fixed?

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ change = {'C': 'G', 'C#': 'G#', 'Db': 'Ab',
           'D': 'A', 'D#': 'Bb', 'Eb': 'Bb',
           'E': 'B', 'F': 'C', 'F#': 'C#',
           'Gb': 'Db', 'G': 'D', 'G#': 'D#',
-          'Ab': 'Eb', 'Bb': 'F', 'B': 'F#'}
+          'Ab': 'Eb', 'A': 'E', 'Bb': 'F',
+          'B': 'F#'}
+
 print("Enter the concert pitches")
 str = input()
+# str = "D E F E C F D E F E C A G D E F E C G F D F E D"
 result = ""
 for x in str.split(' '):
     result += change.get(x) + " "
